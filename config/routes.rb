ea8a0d6607a6cf244e3e435b45f3ffee43018a7a
Betul Erenel <betulerenel@gmail.com>
2024-05-14 22:08:17 -0700

    Added home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     resources :comments
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  root 'home#index'  # Example: This directs the root URL to the index action of the HomeController
+  
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
